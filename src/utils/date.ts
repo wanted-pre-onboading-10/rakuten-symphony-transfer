a7beb3c38d7d49524ad11fe0ac6307e9b4d7aae2
이경은 <2kyung19@gmail.com>
2022-02-24 19:29:40 +0900

    add: 숫자 콤마, 유효기간, 날짜 함수 구현 [#1]

---
+++ src/utils/date.ts
@@ -0,0 +1,22 @@
+export const expireDate = (create: number, expire: number) => {
+  const period = expire - create;
+  if (period < 0) return "만료됨";
+
+  const hour = Math.floor(period / 60 / 60);
+  if (hour >= 48) return `${Math.floor(hour / 24)}일`;
+
+  const min = Math.floor(period / 60 - hour * 60);
+  return `${hour}시간 ${min.toString().padStart(2, "0")}분`;
+};
+
+export const createDate = (create: number) => {
+  const date = new Date(create * 1000);
+
+  const time = date.toTimeString().slice(0, 5);
+  const standard = date.toTimeString().split("+")[1];
+
+  return (
+    `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일 ` +
+    `${time} +${standard.slice(0, 2)}:${standard.slice(2, 4)}`
+  );
+};
